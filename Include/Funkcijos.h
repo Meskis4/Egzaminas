6eddd796098d4ecdc10da47dbabcf9b24cc1bbb2
Meskis4 <147125301+Meskis4@users.noreply.github.com>
2024-05-31 16:13:27 +0300

    Update Funkcijos.h

--- Include/Funkcijos.h
+++ Include/Funkcijos.h
@@ -6,8 +6,10 @@
 
 std::wstring readFile(const std::string& filename);
 
-std::map<std::wstring, int> countWords(const std::wstring& text);
+std::map<std::wstring, int> countWords(const std::wstring& text, std::map<std::wstring, std::set<int>>& wordLines);
+
+bool isNumber(const std::wstring& word);
 
 void GenerateWordCountFile(const std::string& filename, const std::map<std::wstring, int>& wordCountMap);
 
-bool isNumber(const std::wstring& word);
+void GenerateCrossReferenceFile(const std::string& filename, const std::map<std::wstring, std::set<int>>& wordLines, const std::map<std::wstring, int>& wordCountMap);
