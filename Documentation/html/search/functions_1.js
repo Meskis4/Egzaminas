a851e21c148d5a0a1dc203eccafb6a10a0c33ee3
Meskis4 <147125301+Meskis4@users.noreply.github.com>
2024-05-31 20:24:12 +0300

    Add files via upload

---
+++ Documentation/html/search/functions_1.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['extracturls_0',['ExtractURLs',['../_funkcijos_8cpp.html#a753424e8f1a4dedf185ee530e0495b34',1,'ExtractURLs(const std::wstring &amp;text):&#160;Funkcijos.cpp'],['../_funkcijos_8h.html#a753424e8f1a4dedf185ee530e0495b34',1,'ExtractURLs(const std::wstring &amp;text):&#160;Funkcijos.cpp']]]
+];
