bd8c96553d01163733367e7a3d4b348766f9c372
Meskis4 <147125301+Meskis4@users.noreply.github.com>
2024-05-31 16:14:10 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -6,21 +6,22 @@
 using namespace std;
 
 int main() {
-
     SetConsoleOutputCP(CP_UTF8);
-    
+
     string inputFilename = "input.txt";
 
     map<wstring, int> wordCountMap;
+    map<wstring, set<int>> wordLines;
 
-    wordCountMap = countWords(readFile(inputFilename));
+    wordCountMap = countWords(readFile(inputFilename), wordLines);
 
     string outputFilename = "WordCount.txt";
+    string crossRefFilename = "CrossReference.txt";
 
     GenerateWordCountFile(outputFilename, wordCountMap);
-   
+    GenerateCrossReferenceFile(crossRefFilename, wordLines, wordCountMap);
+
     cout << "Programa suveike" << endl;
-  
+
     return 0;
 }
-
